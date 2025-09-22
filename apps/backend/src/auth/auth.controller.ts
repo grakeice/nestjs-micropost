5dc69ffddbc0e87c18f3d7afa0548396a3cf6615
grakeice <89824152+grakeice@users.noreply.github.com>
2025-09-22 15:09:19 +0900

    auth module

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
+
+import { AuthService } from "./auth.service";
 
 @Controller("auth")
-export class AuthController {}
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	@Get()
+	async getAuth(
+		@Query("user_id") name: string,
+		@Query("password") password: string,
+	) {
+		console.log(`${name} has logged in`);
+		return await this.authService.getAuth(name, password);
+	}
+}
--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,8 +1,14 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { Auth } from "@/entities/auth.entity";
+import { User } from "@/entities/user.entity";
+
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { Module } from "@nestjs/common";
 
 @Module({
+	imports: [TypeOrmModule.forFeature([Auth, User])],
 	providers: [AuthService],
 	controllers: [AuthController],
 })
