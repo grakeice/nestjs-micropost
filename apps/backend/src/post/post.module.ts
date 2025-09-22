29c997e23acf7537b62d5e1fa50ef0edb8eb5a28
grakeice <89824152+grakeice@users.noreply.github.com>
2025-09-22 15:33:22 +0900

    post module

--- apps/backend/src/post/post.module.ts
+++ apps/backend/src/post/post.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { Auth } from "@/entities/auth.entity";
+import { MicroPost } from "@/entities/microposts.entity";
 
 import { PostController } from "./post.controller";
 import { PostService } from "./post.service";
 
 @Module({
+	imports: [TypeOrmModule.forFeature([MicroPost, Auth])],
 	controllers: [PostController],
 	providers: [PostService],
 })
