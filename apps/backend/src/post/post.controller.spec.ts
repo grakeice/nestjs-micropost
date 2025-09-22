d5ec617680257d535ef85727ac721516613191da
grakeice <89824152+grakeice@users.noreply.github.com>
2025-09-22 15:47:26 +0900

    testを少し

--- apps/backend/src/post/post.controller.spec.ts
+++ apps/backend/src/post/post.controller.spec.ts
@@ -1,19 +1,32 @@
 import { Test, TestingModule } from "@nestjs/testing";
 
 import { PostController } from "./post.controller";
+import { PostService } from "./post.service";
 
 describe("PostController", () => {
-	let controller: PostController;
+	let service: PostService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [PostController],
+			providers: [
+				{
+					provide: PostService,
+					useValue: {
+						getList: jest.fn().mockReturnValue({}),
+					},
+				},
+			],
 		}).compile();
 
-		controller = module.get<PostController>(PostController);
+		service = module.get<PostService>(PostService);
 	});
 
-	it("should be defined", () => {
-		expect(controller).toBeDefined();
+	it("should be defined", async () => {
+		const controller = new PostController(service);
+		await controller.getList("xxx-xxx-xxx-xxx", 1, 1);
+
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(service.getList).toHaveBeenCalledTimes(1);
 	});
 });
--- apps/backend/src/user/user.controller.spec.ts
+++ apps/backend/src/user/user.controller.spec.ts
@@ -1,19 +1,32 @@
 import { Test, TestingModule } from "@nestjs/testing";
 
 import { UserController } from "./user.controller";
+import { UserService } from "./user.service";
 
 describe("UserController", () => {
-	let controller: UserController;
+	let service: UserService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [UserController],
+			providers: [
+				{
+					provide: UserService,
+					useValue: {
+						getUser: jest.fn().mockReturnValue({}),
+					},
+				},
+			],
 		}).compile();
 
-		controller = module.get<UserController>(UserController);
+		service = module.get<UserService>(UserService);
 	});
 
-	it("should be defined", () => {
-		expect(controller).toBeDefined();
+	it("should be defined", async () => {
+		const controller = new UserController(service);
+		await controller.getUser(1, "xxx-xxx-xxx-xxx");
+
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(service.getUser).toHaveBeenCalledTimes(1);
 	});
 });
