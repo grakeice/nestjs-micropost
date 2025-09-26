0570aacfc0d9d28d34f39cd592196ba43abcee15
grakeice <89824152+grakeice@users.noreply.github.com>
2025-09-26 12:10:42 +0900

    update settings

--- apps/frontend/src/components/ui/form.tsx
+++ apps/frontend/src/components/ui/form.tsx
@@ -2,8 +2,6 @@
 
 import * as React from "react";
 
-import { Label } from "#/components/ui/label";
-import { cn } from "#/lib/utils";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { Slot } from "@radix-ui/react-slot";
 import {
@@ -15,6 +13,9 @@ import {
 	type FieldPath,
 	type FieldValues,
 } from "react-hook-form";
+
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
 
 const Form = FormProvider;
 
--- apps/frontend/src/components/ui/label.tsx
+++ apps/frontend/src/components/ui/label.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 
-import { cn } from "#/lib/utils";
 import * as LabelPrimitive from "@radix-ui/react-label";
+
+import { cn } from "@/lib/utils";
 
 function Label({
 	className,
