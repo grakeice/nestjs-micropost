f958feba3995a6092a2957e2195b1f2cc4179704
grakeice <89824152+grakeice@users.noreply.github.com>
2025-10-01 12:37:31 +0900

    page: ユーザー編集ページの追加

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/main";
 import SignInPage from "./pages/signin";
 import SignUpPage from "./pages/signup";
+import UserPage from "./pages/user";
 import { UserProvider } from "./providers/UserProvider";
 
 function App(): JSX.Element {
@@ -15,6 +16,7 @@ function App(): JSX.Element {
 					<Route path="/" element={<SignInPage />} />
 					<Route path="/signup" element={<SignUpPage />} />
 					<Route path="/main" element={<MainPage />} />
+					<Route path="/user" element={<UserPage />} />
 				</Routes>
 			</UserProvider>
 		</>
---
+++ apps/frontend/src/pages/user.tsx
@@ -0,0 +1,5 @@
+import type { JSX } from "react";
+
+export default function UserPage(): JSX.Element{
+	return <div>ユーザー編集ページだよ〜</div>
+}
